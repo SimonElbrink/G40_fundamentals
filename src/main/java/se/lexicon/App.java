6087f76f64a6a8fc581962ff05b863c67200c516
Simon Elbrink <simon.elbrink@gmail.com>
2021-11-30 11:29:32 +0100

    After lecture "Fundamentals Part 1"

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,6 +1,12 @@
 package se.lexicon;
 
-/**
+// Single line comment
+
+/*
+    multiline comment
+ */
+
+/**  Multiline comment (Documentation)
  * Hello world!
  *
  */
@@ -8,6 +14,51 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        char numberSign = '\u0023';
+        char four = '\u0034';
+        char zero = '\u0030';
+
+        System.out.println("Hello Group " + numberSign + four + zero);
+
+
+        int number1 = 100;
+        int number2 = 50;
+        int result = 100;
+
+//        number1 = number2;
+        result = result + number1 + number2;
+
+        System.out.println(result);
+
+
+        System.out.println(++number1);
+
+        System.out.println(12 % 3);
+
+
+        boolean isTrue = true;
+
+        System.out.println(isTrue);
+
+
+
+        String firstName = "Simon";
+
+        System.out.println(firstName);
+
+
     }
+
+    /**
+     * Create a String with your first name as content.
+     * Print out the String object to the console.
+     */
+    public static void practice1 (){
+        String firstName = "Simon";
+
+        System.out.println(firstName);
+    }
+
+
+
 }
