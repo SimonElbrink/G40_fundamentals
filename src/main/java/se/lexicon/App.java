483a57370f87939a2f7452cd531c6787d91976ad
Simon Elbrink <simon.elbrink@gmail.com>
2021-12-01 09:28:31 +0100

    Review and practice from lecture slides.

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,4 +1,4 @@
-package se.lexicon;
+package se.lexicon; // Package always on line 1
 
 // Single line comment
 
@@ -10,17 +10,27 @@ package se.lexicon;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        char numberSign = '\u0023';
-        char four = '\u0034';
-        char zero = '\u0030';
+public class App { //start of the class
+
+    /**
+     * Main method, needed to run the program.
+     * where of program lives and dies.
+     * if your code is not included in the main in any way it will not be executed.
+     */
+    public static void main( String[] args ) //Syntax for writhing a main method.
+    { //Start of main method
+
+        System.out.println("#### chars ####");
+        char numberSign = '\u0023'; //#
+        char four = '\u0034'; //4
+        char zero = '\u0030'; //0
+
+        char A = 'A';
 
         System.out.println("Hello Group " + numberSign + four + zero);
 
 
+        System.out.println("#### int numbers ####");
         int number1 = 100;
         int number2 = 50;
         int result = 100;
@@ -36,16 +46,25 @@ public class App
         System.out.println(12 % 3);
 
 
+        System.out.println("#### boolean ####");
         boolean isTrue = true;
 
         System.out.println(isTrue);
 
 
 
+        System.out.println("#### PRACTICE 1 ####");
         practice1();
 
+        System.out.println("#### PRACTICE 2 ####");
+        practice2();
 
-    }
+
+        System.out.println("#### PRACTICE 3 ####");
+
+        practice3();
+
+    } //End of main method
 
     /**
      * Create a String with your first name as content.
@@ -57,6 +76,34 @@ public class App
         System.out.println(firstName);
     }
 
+    /**
+     * Create two Strings.
+     * One with your first name and the other with your last name.
+     * Print out both Strings to the console with a ” ” separating them.
+     */
+    public static void practice2(){
+
+        String firstName = "Simon";
+        String lastName = "Elbrink";
+
+        System.out.println(firstName + " " + lastName);
+    }
+
+    /**
+     * Create two variables with decimal values.
+     * Create a third decimal variable called result.
+     * Assign the sum of the two first variables to result.
+     */
+    public static void practice3(){
+        double number1 = 50.66;
+        double number2 = 25.96;
+        double result;
+
+        result = number1 + number2;
+
+        System.out.println(result);
+    }
 
 
-}
+
+} // End of class
